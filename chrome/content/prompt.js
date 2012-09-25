2f4d4f0dfc38651592e64b7b30343fbdbe977d12
jik <jik@93042d25-c76b-44c9-be46-c43815ad44d8>
2012-09-25 13:47:04 +0000

    Upversion to 4.0.5beta2.
    Add back the ability to choose the date and time in the prompt pop-up
    window using the mouse.

--- chrome/content/prompt.js
+++ chrome/content/prompt.js
@@ -75,30 +75,9 @@ var Sendlater3Prompt = {
 
     SetOnLoad: function() {
         SL3U.Entering("Sendlater3Prompt.SetOnLoad");
-	document.getElementById("sendlater3-yearvalue")
-	    .addEventListener("ValueChange",
-			      Sendlater3Prompt.populateMonths, false);
-	document.getElementById("sendlater3-monthvalue")
-	    .addEventListener("ValueChange",
-			      Sendlater3Prompt.populateDays, false);
-	document.getElementById("sendlater3-dayvalue")
-	    .addEventListener("ValueChange",
-			      Sendlater3Prompt.updateSummary, false);
-	document.getElementById("sendlater3-hourvalue").
-	    addEventListener("ValueChange",
-			     Sendlater3Prompt.updateSummary, false);
-	document.getElementById("sendlater3-minvalue").
-	    addEventListener("ValueChange",
-			     Sendlater3Prompt.updateSummary, false);
 	document.getElementById("sendlater3-time-text")
 	    .addEventListener("ValueChange",
 			     Sendlater3Prompt.updateSummary, false);
-	Sendlater3Prompt.populateYears();
-	Sendlater3Prompt.populateHours();
-	Sendlater3Prompt.populateMins();
-	var hhmm = new Date();
-	document.getElementById("sendlater3-hourvalue").value = hhmm.getHours();
-	document.getElementById("sendlater3-minvalue").value = hhmm.getMinutes();
 	var i;
 	for (i = 1; i <= 3; i++) {
 	    document.getElementById("sendlater3-shortcutbtn_"+i).label=SL3U.ButtonLabel(i);
@@ -174,164 +153,59 @@ var Sendlater3Prompt = {
 	if (prevXSendLater) {
 	   document.getElementById("sendlater3-time-text").value =
 	       prevXSendLater.format("long", sugarLocale());
-	   document.getElementById("sendlater3-yearvalue").value =
-	       prevXSendLater.getFullYear();
-	   document.getElementById("sendlater3-monthvalue").value =
-	       prevXSendLater.getMonth();
-	   document.getElementById("sendlater3-dayvalue").value =
-	       prevXSendLater.getDate();
-	   document.getElementById("sendlater3-hourvalue").value =
-	       prevXSendLater.getHours();
-	   document.getElementById("sendlater3-minvalue").value =
-	       prevXSendLater.getMinutes();
-	    
 	}
-	if (document.getElementById("sendlater3-time-deck").selectedIndex == 0) {
-	    document.getElementById("sendlater3-time-text").focus();
-	}
-	else {
-	    document.getElementById("sendlater3-hourvalue").focus();
-	}
+	document.getElementById("sendlater3-time-text").focus();
 	Sendlater3Prompt.AddControlReturnListeners(document);
         SL3U.Leaving("Sendlater3Prompt.SetOnLoad");
     },
 
-    populateYears: function() {
-        SL3U.Entering("Sendlater3Prompt.populateYears");
-	var today = new Date();
-	var container = document.getElementById("sendlater3-years");
-	var i;
-	for (i=0;i<5;i++)
-	{
-	      var newitem = document.createElement("menuitem");
-	      newitem.setAttribute("label",(today.getFullYear()+i).toString());
-	      newitem.setAttribute("value",(today.getFullYear()+i).toString());
-	      container.appendChild(newitem);
-	}
-
-	document.getElementById("sendlater3-yearvalue").selectedIndex = 0;
-        SL3U.Leaving("Sendlater3Prompt.populateYears");
+    pickersToText: function() {
+	SL3U.Entering("Sendlater3Prompt.pickersToText");
+	var textField = document.getElementById("sendlater3-time-text");
+	var datePicker = document.getElementById("sendlater3-datepicker");
+	var timePicker = document.getElementById("sendlater3-timepicker");
+	var date = datePicker.value;
+	var time = timePicker.value;
+	// Strip seconds from time
+	time = time.replace(/(.*:.*):.*/, "$1");
+	textField.value = date + " " + time;
+	Sendlater3Prompt.updateSummary(true);
+	SL3U.Leaving("Sendlater3Prompt.pickersToText");
     },
 
-    populateMonths: function() {
-        SL3U.Entering("Sendlater3Prompt.populateMonths");
-	var selectedyear =  document.getElementById("sendlater3-yearvalue").value;
-	var today = new Date();
-	var monthStr = [ SL3U.PromptBundleGet("January"),
-	    	         SL3U.PromptBundleGet("February"),
-			 SL3U.PromptBundleGet("March"),
-			 SL3U.PromptBundleGet("April"),
-			 SL3U.PromptBundleGet("May"),
-			 SL3U.PromptBundleGet("June"),
-			 SL3U.PromptBundleGet("July"),
-			 SL3U.PromptBundleGet("August"),
-			 SL3U.PromptBundleGet("September"),
-			 SL3U.PromptBundleGet("October"),
-			 SL3U.PromptBundleGet("November"),
-			 SL3U.PromptBundleGet("December") ];
-	var container = document.getElementById("sendlater3-months");
-	Sendlater3Prompt.clearChildren(container);
-	var i = 0;
-	if (selectedyear == today.getFullYear()) {  
-	    i = today.getMonth();
-	}
-	for (;i<12;i++) {
-	    var newitem = document.createElement("menuitem");
-	    newitem.setAttribute("label",monthStr[i]);
-	    newitem.setAttribute("value",i);
-	    container.appendChild(newitem);
-	}
-	document.getElementById("sendlater3-monthvalue").selectedIndex = 0;
-        SL3U.Leaving("Sendlater3Prompt.populateMonths");
+    dateToPickers: function(dateObj) {
+	var datePicker = document.getElementById("sendlater3-datepicker");
+	var timePicker = document.getElementById("sendlater3-timepicker");
+	datePicker.value = dateObj.format("{yyyy}-{MM}-{dd}");
+	timePicker.value = dateObj.format("{HH}:{mm}");
     },
 
-    populateDays: function() {
-        SL3U.Entering("Sendlater3Prompt.populateDays");
-	var today = new Date();
-
-	var selectedyear =  document.getElementById("sendlater3-yearvalue").value;
-	var selectedmonth =  document.getElementById("sendlater3-monthvalue").value;
-
-	var container = document.getElementById("sendlater3-days");
-	Sendlater3Prompt.clearChildren(container);
-	var i=0;
-        if ((selectedyear == today.getFullYear()) &&
-            (selectedmonth == today.getMonth())) {
-	    i = today.getDate() - 1;
-	}
-	for (;i<Sendlater3Prompt.getMaxDays(selectedyear,selectedmonth);i++) {
-	    var newitem = document.createElement("menuitem");
-	    newitem.setAttribute("label",(i+1).toString());
-	    newitem.setAttribute("value",(i+1).toString());
-	    container.appendChild(newitem);
-	}
-	document.getElementById("sendlater3-dayvalue").selectedIndex = 0;
-        SL3U.Leaving("Sendlater3Prompt.populateDays");
-    },
-
-    populateHours: function() {
-        SL3U.Entering("Sendlater3Prompt.populateHours");
-	var container = document.getElementById("sendlater3-hours");
-	var i;
-	for (i=0;i<24;i++) {
-	    var newitem = document.createElement("menuitem");
-	    newitem.setAttribute("label",SL3U.DZFormat(i));
-	    newitem.setAttribute("value",i.toString());
-	    container.appendChild(newitem);
-	}
-        SL3U.Leaving("Sendlater3Prompt.populateHours");
-    },
-
-    populateMins: function() {
-        SL3U.Entering("Sendlater3Prompt.populateMins");
-	var container = document.getElementById("sendlater3-mins");
-	var i;
-	for (i=0;i<60;i++) {
-	    var newitem = document.createElement("menuitem");
-	    newitem.setAttribute("label",SL3U.DZFormat(i));
-	    newitem.setAttribute("value",i.toString());
-	    container.appendChild(newitem);
-	}
-        SL3U.Leaving("Sendlater3Prompt.populateMins");
-    },
-
-    updateSummary: function() {
+    updateSummary: function(fromPicker) {
         SL3U.Entering("Sendlater3Prompt.updateSummary");
-	var whichUI = document.getElementById("sendlater3-time-deck").selectedIndex;
 	var dateObj;
-	if (whichUI == 0) {
-	    var dateStr = document.getElementById("sendlater3-time-text").value;
-	    if (dateStr) {
-		try {
-		    var dateObj = dateParse(dateStr);
-		}
-		catch (ex) {
-		}
-		if (! (dateObj && dateObj.isValid())) {
-		    dateObj = null;
-		}
+	var dateStr = document.getElementById("sendlater3-time-text").value;
+	if (dateStr) {
+	    try {
+		var dateObj = dateParse(dateStr);
 	    }
-	}
-	else {
-	    var selectedyear =  document.getElementById("sendlater3-yearvalue").value;
-	    var selectedmonth =  document.getElementById("sendlater3-monthvalue").value;
-	    var selecteddate =  document.getElementById("sendlater3-dayvalue").value;
-	    var selectedhour =  document.getElementById("sendlater3-hourvalue").value;
-	    var selectedmin =  document.getElementById("sendlater3-minvalue").value;
-	    var dateObj = SL3U.toSendDate(selectedyear, selectedmonth, selecteddate,
-					  selectedhour, selectedmin);
+	    catch (ex) {
+	    }
+	    if (! (dateObj && dateObj.isValid())) {
+		dateObj = null;
+	    }
 	}
 	var button = document.getElementById("sendlater3-callsendat");
 	if (dateObj) {
 	    button.label = SL3U.PromptBundleGet("sendaround") + " "
 		+ dateToSugarDate(dateObj).format('long', sugarLocale());
 	    Sendlater3Prompt.CheckRecurring(dateObj);
+	    if (! fromPicker) {
+		Sendlater3Prompt.dateToPickers(dateObj);
+	    }
 	}
 	else {
 	    button.label = SL3U.PromptBundleGet("entervalid");
 	}
-//	document.getElementById("sendlater3-summary").value =
-//	    SL3U.PromptBundleGet("willsendat") + " " + displayStr;
 	document.getElementById("sendlater3-callsendat")
 	    .setAttribute("disabled", ! dateObj);
         SL3U.Returning("Sendlater3Prompt.updateSummary", dateObj);
