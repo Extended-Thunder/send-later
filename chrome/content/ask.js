9254c00d33b92f6b793f84362f044d95136c6fbb
Jonathan Kamens <jik@quantopian.com>
2018-04-09 15:45:55 -0400

    Update donation link

--- chrome/content/ask.js
+++ chrome/content/ask.js
@@ -1,5 +1,5 @@
 function Sendlater3AskDonate() {
-    var url = "https://addons.mozilla.org/thunderbird/addon/send-later-3/contribute/roadblock/?src=send-ask";
+    var url = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=jik%40kamens%2eus&lc=US&item_name=Send%20Later%20add%2don%20%28%245%2e00%20recommended%20donation%2c%20but%20give%20what%20you%27d%20like%21%29&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donat";
     var protocolSvc = Components
 	.classes["@mozilla.org/uriloader/external-protocol-service;1"]
         .getService(Components.interfaces.nsIExternalProtocolService);
