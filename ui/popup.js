f0c55b2a5e88458232875f70e3ba024205089e51
Jonathan Perry-Houts <75693008+jph-sendlater@users.noreply.github.com>
2020-12-09 13:55:20 -0800

    Warn user before using "place in outbox" and "send now" buttons

--- ui/popup.js
+++ ui/popup.js
@@ -41,8 +41,9 @@ const SLPopup = {
       action: "doSendNow"
     };
     SLStatic.debug(message);
-    browser.runtime.sendMessage(message).catch(SLStatic.error);
-    setTimeout((() => window.close()), 150);
+    browser.runtime.sendMessage(message).catch(
+      (err) => SLStatic.error(err)
+    );
   },
 
   doPlaceInOutbox() {
@@ -51,8 +52,9 @@ const SLPopup = {
       action: "doPlaceInOutbox"
     };
     SLStatic.debug(message);
-    browser.runtime.sendMessage(message).catch(SLStatic.error);
-    setTimeout((() => window.close()), 150);
+    browser.runtime.sendMessage(message).catch(
+      (err) => SLStatic.trace(err)
+    );
   },
 
   domElementsAsArray() {
