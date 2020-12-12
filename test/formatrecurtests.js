0946c17dd8f464e152d2b45714dac77f50f0b381
Jonathan Perry-Houts <75693008+jph-sendlater@users.noreply.github.com>
2020-12-12 10:03:36 -0800

    Handle recur spec restrictions on one-off messages. Add unit tests.

--- test/formatrecurtests.js
+++ test/formatrecurtests.js
@@ -10,16 +10,19 @@ exports.init = function() {
   }
 
   const tests = [
+    ["none between 730 1930", "betw. 7:30 and 19:30"],
+    ["none on 1 3 5", "Only on Monday, Wednesday, and Friday"],
+    ["none between 100 600 on 1 3 6", "betw. 1:00 and 6:00 Only on Monday, Wednesday, and Saturday"],
     ["minutely", "Recur minutely"],
     ["daily", "Recur daily"],
     ["weekly", "Recur weekly"],
     ["monthly 3", "Recur monthly"],
-    ["monthly 0 3", "Recur every 3rd Sunday"],
+    ["monthly 0 3", "Recur monthly, 3rd Sunday of the month"],
     ["yearly 10 5", "Recur yearly"],
     ["function froodle", 'Recur according to function “froodle”'],
     ["minutely / 5", "Recur every 5 minutes"],
     ["minutely between 830 1730", "Recur minutely betw. 8:30 and 17:30"],
-    ["minutely on 1 2 3", "Recur minutely on Monday, Tuesday, and Wednesday"]
+    ["minutely on 1 2 3", "Recur minutely Only on Monday, Tuesday, and Wednesday"]
   ];
   for (const test of tests) {
     SLTests.AddTest("FormatRecur " + test[0], FormatRecurTest, test);
