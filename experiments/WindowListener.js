f4cf5a293b2706f65c9256fb4367dba5509e5680
Jonathan Perry-Houts <75693008+jph-sendlater@users.noreply.github.com>
2021-05-08 09:02:35 -0700

    Revert "Ensure external scripts hash properly with upstream version"
    This reverts commit 427c7f86214433216bf2bc3296fb3d709f976534.

--- experiments/WindowListener.js
+++ experiments/WindowListener.js
@@ -138,12 +138,8 @@
  */
 
 // Import some things we need.
-var { ExtensionCommon } = ChromeUtils.import(
-  "resource://gre/modules/ExtensionCommon.jsm"
-);
-var { ExtensionSupport } = ChromeUtils.import(
-  "resource:///modules/ExtensionSupport.jsm"
-);
+var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
+var { ExtensionSupport } = ChromeUtils.import("resource:///modules/ExtensionSupport.jsm");
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 var WindowListener = class extends ExtensionCommon.ExtensionAPI {
