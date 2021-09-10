29bd6d2253a3a949f6ac096fa77714dd6d010dcd
Jonathan Perry-Houts <75693008+jph-sendlater@users.noreply.github.com>
2021-09-10 11:34:13 -0700

    Add tests for estimated send time function

--- test/miscellaneoustests.js
+++ test/miscellaneoustests.js
@@ -16,4 +16,34 @@ exports.init = function() {
     const result = SLStatic.unparseArgs(input);
     return (result === expected) || `Expected "${expected}", got "${result}"`;
   }, [[1,2,"foo",[1,"bar"], 6], '1, 2, "foo", [1, "bar"], 6']);
+
+  function testEstimatedSendTime(scheduledDate, previousLoop, loopMinutes) {
+    scheduledDate = SLStatic.floorDateTime(scheduledDate);
+
+    let result = SLStatic.estimateSendTime(scheduledDate, previousLoop, loopMinutes);
+
+    if (result.getTime() < Date.now())
+      return `Estimated send time is in the past: ${result}`;
+
+    if (result.getTime() < scheduledDate.getTime())
+      return `Estimated send time is before scheduled send time: ${result}`;
+
+    if (result.getTime()-scheduledDate.getTime() > loopMinutes*60000
+        && result.getTime()-Date.now() > loopMinutes*60000)
+      return `Estimated send time is more than ${loopMinutes} past scheduled time.`;
+
+    return true;
+  }
+
+  SLTests.AddTest("Test estimate send time", testEstimatedSendTime,
+    [new Date(), new Date(Date.now()), 1]);
+
+  SLTests.AddTest("Test estimate send time 2", testEstimatedSendTime,
+    [new Date(), new Date(Date.now()-30e3), 1]);
+
+  SLTests.AddTest("Test estimate send time 3", testEstimatedSendTime,
+    [new Date(), new Date(Date.now()-300e3), 1]);
+
+  SLTests.AddTest("Test estimate send time 4", testEstimatedSendTime,
+    [new Date(), new Date(Date.now()-120e3), 5]);
 }
