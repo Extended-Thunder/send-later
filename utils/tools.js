e433684a4ee2fb61f60b26f471438ad173508d73
Jonathan Perry-Houts <75693008+jph-sendlater@users.noreply.github.com>
2022-12-23 16:54:44 -0800

    Reduce number of message read operations with caching

--- utils/tools.js
+++ utils/tools.js
@@ -180,16 +180,35 @@ var SLTools = {
     });
   },
 
+  scheduledMsgCache: new Set(),
+  unscheduledMsgCache: new Set(),
+
   // Count draft messages containing the correct `x-send-later-uuid` header.
   async countActiveScheduledMessages() {
+    let start = performance.now();
     const preferences = await SLTools.getPrefs();
     let isScheduled = await SLTools.forAllDrafts(
       async (msgHdr) => {
-        let fullMsg = await messenger.messages.getFull(msgHdr.id);
-        let uuid = (fullMsg.headers["x-send-later-uuid"]||[])[0];
-        return uuid == preferences.instanceUUID;
-      }
+        if (SLTools.scheduledMsgCache.has(msgHdr.id)) {
+          return true;
+        } else if (SLTools.unscheduledMsgCache.has(msgHdr.id)) {
+          return false;
+        } else {
+          let fullMsg = await messenger.messages.getFull(msgHdr.id);
+          let uuid = (fullMsg.headers["x-send-later-uuid"]||[])[0];
+          if (uuid == preferences.instanceUUID) {
+            SLTools.scheduledMsgCache.add(msgHdr.id);
+            return true;
+          } else {
+            SLTools.unscheduledMsgCache.add(msgHdr.id);
+            return false;
+          }
+        }
+      },
+      true // Running this sequentially seems to give slightly better performance.
     )
-    return isScheduled.filter(x => x).length;
+    let nActive = isScheduled.filter(x => x).length;
+    console.debug(`countActiveScheduledMessages: ${performance.now()-start} ms`);
+    return nActive;
   },
 };
