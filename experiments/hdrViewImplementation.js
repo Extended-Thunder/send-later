d6e59fd9e666897714281e8b1c6736b71b0e6349
Jonathan Kamens <jik@kamens.us>
2023-08-08 14:17:56 -0400

    Make the Send Later header display on messages in the 3pane window
    The current format the header is displayed in isn't ideal, and there
    are some other TODOs remaining here, but this is good enough to ship
    and it can perhaps tide us over until there's a way to do custom
    headers without an experiment.

--- experiments/hdrViewImplementation.js
+++ experiments/hdrViewImplementation.js
@@ -4,14 +4,18 @@ var ExtensionSupport = globalThis.ExtensionSupport || ChromeUtils.import(
 ).ExtensionSupport;
 
 class CustomHdrRow {
-  constructor(context, name, tooltip) {
+  constructor(context, name, value) {
     this.context = context;
     this.name = name;
-    this.tooltip = tooltip;
+    this.value = value;
     this.rowId = ExtensionCommon.makeWidgetId(
       `${context.extension.id}-${name}-custom-hdr`
     );
-    this.handlers = new Set();
+  }
+
+  getDocument(window) {
+    return window.gTabmail.currentAbout3Pane.document.
+      getElementById("messageBrowser").contentDocument;
   }
 
   destroy() {
@@ -20,14 +24,7 @@ class CustomHdrRow {
 
     for (let window of Services.wm.getEnumerator("mail:3pane")) {
       try {
-        window.document.getElementById(this.rowId).remove();
-
-        for (let i = 0; i < window.gMessageListeners.length; i++) {
-          if (window.gMessageListeners[i].id === this.name) {
-            window.gMessageListeners.splice(i, 1);
-            console.log('Removed gMessageListener for header row:',this.name);
-          }
-        }
+        this.getDocument(window).getElementById(this.rowId).remove();
       } catch (ex) {
         console.error(ex);
       }
@@ -53,79 +50,62 @@ class CustomHdrRow {
   }
 
   addToWindow(window) {
-    // Note this changed in TB96 and TB102, but the current strict
-    // min version should prevent this version of the extension from
-    // running prior to that, so we don't implement any fall-backs.
-
-    let document = window.document;
+    let document = this.getDocument(window);
 
     // If a row already exists, do not create another one
     let newRowNode = document.getElementById(this.rowId);
     if (!newRowNode) {
       // Create new collapsed row
+      // TODO Logic copied from SimpleHeaderRow in mail/base/content/widgets/
+      // header-fields.js. I wish I knew how to just import that code so I could
+      // do document.createElement("simple-header-row") and have all this happen
+      // automatically!
+      // TODO The row heading and value are displaying on separate lines. I
+      // don't know why or what the right way to fix it is.
+      // TODO In the DOM this node shows up as "div" instead of "html:div". I'm
+      // not sure whether this matters or how to fix it if it does matter.
+      // I believe using `createElementNS` rather than `createElement` is
+      // intended to avoid this problem, but it's not working.
       newRowNode = document.createElementNS(
         "http://www.w3.org/1999/xhtml",
-        "div"
+        "div");
+      newRowNode.id = this.rowId;
+      newRowNode.dataset.headerName = this.rowId;
+      newRowNode.dataset.prettyHeaderName = this.name;
+      newRowNode.setAttribute("is", "simple-header-row");
+
+      let heading = document.createElement("span");
+      heading.id = `${newRowNode.dataset.headerName}Heading`;
+      heading.classList.add("row-heading");
+      let sep = document.createElement("span");
+      sep.classList.add("screen-reader-only");
+      sep.setAttribute("data-l10n-name", "field-separator");
+      heading.appendChild(sep);
+      document.l10n.setAttributes(
+        heading,
+        "message-header-custom-field",
+        {
+          fieldName: newRowNode.dataset.prettyHeaderName,
+        }
       );
-      newRowNode.setAttribute("id", this.rowId);
-      newRowNode.classList.add("message-header-row");
+      newRowNode.appendChild(heading);
 
-      let newLabelNode = document.createXULElement("label");
-      newLabelNode.setAttribute("id", `${this.rowId}-label`);
-      newLabelNode.setAttribute("value", this.name);
-      newLabelNode.setAttribute("class", "message-header-label");
+      newRowNode.classList.add("header-row");
+      newRowNode.tabIndex = 0;
 
-      newRowNode.appendChild(newLabelNode);
-
-      // Create and append the new header value.
-      let newHeaderNode = document.createElement("div", {
-        is: "simple-header-row",
-      });
-      newHeaderNode.setAttribute("id", `${this.rowId}-content`);
-      newHeaderNode.dataset.prettyHeaderName = this.name;
-      newHeaderNode.dataset.headerName = this.rowId;
-      newRowNode.appendChild(newHeaderNode);
+      let valueNode = document.createElement("span");
+      valueNode.id = `${this.rowId}-value`;
+      valueNode.textContent = this.value;
+      newRowNode.appendChild(valueNode);
 
       // Add the new row to the extra headers container.
       let topViewNode = document.getElementById("extraHeadersArea")
       topViewNode.appendChild(newRowNode);
     }
-
-    for (let handler of this.handlers)
-      this.addMsgListenerToWindow(window, handler);
-  }
-
-  async addMsgListenerToCurrentWindows(fire) {
-    for (let window of Services.wm.getEnumerator("mail:3pane")) {
-      await CustomHdrRow.waitForWindow(window);
-      this.addMsgListenerToWindow(window, fire);
+    else {
+      let valueNode = document.getElementById(`${this.rowId}-value`);
+      valueNode.textContent = this.value;
     }
-  }
-
-  addMsgListenerToWindow(window, handler) {
-    this.handlers.add(handler);
-    let name = this.name;
-    let msgListener = {
-      id: name,
-      onStartHeaders() {},
-      onEndHeaders() {}
-    };
-    msgListener.onBeforeShowHeaderPane = () => {
-      window.document.getElementById(this.rowId).hidden = true;
-      if (window.gDBView) {
-        try {
-          let msgHdr = window.gDBView.hdrForFirstSelectedMessage;
-          if (msgHdr) {
-            let hdr = this.context.extension.messageManager.convert(msgHdr);
-            handler.async(hdr).then(result => {
-              window.document.getElementById(this.rowId).hidden = !result.visible;
-              window.document.getElementById(`${this.rowId}-content`).headerValue = result.text;
-            }).catch(console.error);
-          }
-        } catch (ex) {}
-      }
-    };
-    window.gMessageListeners.push(msgListener);
   }
 }
 
@@ -138,60 +118,36 @@ var headerView = class extends ExtensionCommon.ExtensionAPI {
           console.error("Unable to destroy hdrRow:",ex);
         }
       }
-
-      ExtensionSupport.unregisterWindowListener("customHdrRowWL");
     }
 
     getAPI(context) {
       context.callOnClose(this);
-
-      ExtensionSupport.registerWindowListener("customHdrRowWL",
-        {
-          chromeURLs: ["chrome://messenger/content/messenger.xhtml"],
-          async onLoadWindow(window) {
-            await CustomHdrRow.waitForWindow(window);
-            for (let hdrRow of hdrRows.values())
-              hdrRow.addToWindow(window);
-          }
-        });
-
       let hdrRows = new Map();
       this.hdrRows = hdrRows;
 
       return {
         headerView: {
 
-          async addCustomHdrRow({ name, tooltip }) {
-            if (hdrRows.has(name))
-              throw new ExtensionUtils.ExtensionError("Cannot add hdrRows with the same name");
-            let hdrRow = new CustomHdrRow(context, name, tooltip);
+          async addCustomHdrRow({ name, value }) {
+            let hdrRow = hdrRows.get(name);
+            if (hdrRow) {
+              hdrRow.value = value;
+            }
+            else {
+              hdrRow = new CustomHdrRow(context, name, value);
+            }
             await hdrRow.addToCurrentWindows();
             hdrRows.set(name, hdrRow);
           },
-  
+
           async removeCustomHdrRow(name) {
             let hdrRow = hdrRows.get(name);
             if (!hdrRow)
               throw new ExtensionUtils.ExtensionError("Cannot remove non-existent hdrRow");
-              hdrRow.destroy();
+            hdrRow.destroy();
             hdrRows.delete(name);
-          },
-
-          onHeaderRowUpdate: new ExtensionCommon.EventManager({
-            context,
-            name: "headerView.onHeaderRowUpdate",
-            register: (fire, name) => {
-              let hdrRow = hdrRows.get(name);
-              if (!hdrRow) {
-                throw new ExtensionUtils.ExtensionError(
-                  "Cannot add a hdrRow fill handler for a hdrRow that has not been defined"
-                );
-              }
-              hdrRow.addMsgListenerToCurrentWindows(fire).catch(console.error);
-              return () => {};
-            },
-          }).api()
+          }
         }
       }
     }
-  };
+}
