45254848130aef0d3597d9c5563271660d660c5d
Jonathan Kamens <jik@kamens.us>
2023-08-12 13:07:21 -0400

    Fix prettierjs invocation in Emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
 ((js-mode . ((js-indent-level . 2)
              (js-switch-indent-offset . 2)
+             (prettier-js-command . "npx")
+             (prettier-js-args . ("prettier"))
              (mode . prettier-js))))
