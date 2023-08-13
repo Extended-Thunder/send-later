Extended-Thunder/send-later

commit 066d90e52e9916b51b37aecf50907ce147e76c69

Make Ctrl-Click and Shift-Click on the compose action button work
You're supposed to be able to bind shortcuts that get activated if you
click the compose action button while holding down Ctrl or Shift, but
that was broken. It's fixed now. The issue was that the click events
don't get delivered when there's a popup bound to the button, so we
have to leave the popup unbound most of the time, then when we get a
click, first check if one of the modifier keys was held down, and if
not, then add the popup to the button for long enough to open it and
trigger the open. This is as partial fix for #583.

background.js
@@ -844,7 +844,6 @@ const SendLater = {
       SendLater.onMessageDisplayedListener,
     );
     messenger.commands.onCommand.addListener(SendLater.onCommandListener);
-    messenger.composeAction.setPopup({ popup: "ui/popup.html" });
     messenger.composeAction.onClicked.addListener(
       SendLater.clickComposeListener,
     );
@@ -1466,7 +1465,16 @@ const SendLater = {
       );
       await SendLater.quickSendWithUfunc(funcName, funcArgs, tab.id);
     } else {
-      await messenger.composeAction.openPopup();
+      // The onClicked event on the compose action button doesn't fire if a
+      // pop-up is configured, so we have to set and open the popup here and
+      // then immediately unset the popup so that we can catch the key binding
+      // if the user clicks again with a modifier.
+      messenger.composeAction.setPopup({ popup: "ui/popup.html" });
+      try {
+        await messenger.composeAction.openPopup();
+      } finally {
+        messenger.composeAction.setPopup({ popup: null });
+      }
     }
   },
 

commit e2452da894485141e3e679c65e39baac0e56257b

Fix Ctrl-Shift-Enter et al that were broken in 066d90e

background.js
@@ -1021,6 +1021,20 @@ const SendLater = {
     }
   },
 
+  async openPopup() {
+    SLStatic.info("Opening popup");
+    // The onClicked event on the compose action button doesn't fire if a
+    // pop-up is configured, so we have to set and open the popup here and
+    // then immediately unset the popup so that we can catch the key binding
+    // if the user clicks again with a modifier.
+    messenger.composeAction.setPopup({ popup: "ui/popup.html" });
+    try {
+      await messenger.composeAction.openPopup();
+    } finally {
+      messenger.composeAction.setPopup({ popup: null });
+    }
+  },
+
   // Custom events that are attached to user actions within
   // composition windows. These events occur when the user activates
   // the built-in send or send later using either key combinations
@@ -1030,8 +1044,7 @@ const SendLater = {
     switch (keyid) {
       case "key_altShiftEnter": {
         if (SendLater.prefCache.altBinding) {
-          SLStatic.info("Opening popup");
-          await messenger.composeAction.openPopup();
+          await SendLater.openPopup();
         } else {
           SLStatic.info(
             "Ignoring Alt+Shift+Enter on account of user preferences",
@@ -1054,23 +1067,21 @@ const SendLater = {
             });
           }
         } else {
-          SLStatic.info("Opening popup");
-          await messenger.composeAction.openPopup();
+          await SendLater.openPopup();
         }
         break;
       }
       case "cmd_sendLater": {
         // User clicked the "Send Later" menu item, which should always
         // open the Send Later popup.
-        await messenger.composeAction.openPopup();
+        await SendLater.openPopup();
         break;
       }
       case "cmd_sendNow":
       case "cmd_sendButton":
       case "key_send": {
         if (SendLater.prefCache.sendDoesSL) {
-          SLStatic.debug("Opening scheduler dialog.");
-          await messenger.composeAction.openPopup();
+          await SendLater.openPopup();
         } else if (SendLater.prefCache.sendDoesDelay) {
           // Schedule with delay.
           const sendDelay = SendLater.prefCache.sendDelay;
@@ -1469,12 +1480,7 @@ const SendLater = {
       // pop-up is configured, so we have to set and open the popup here and
       // then immediately unset the popup so that we can catch the key binding
       // if the user clicks again with a modifier.
-      messenger.composeAction.setPopup({ popup: "ui/popup.html" });
-      try {
-        await messenger.composeAction.openPopup();
-      } finally {
-        messenger.composeAction.setPopup({ popup: null });
-      }
+      await SendLater.openPopup();
     }
   },
 