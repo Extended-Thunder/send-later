7b15dd6cf71f6a11c47320c75ed309dff0d8f50c
John Bieling <john.bieling@gmx.de>
2024-07-30 09:01:40 +0200

    Revert "Restore execution order related to compactDrafts()"
    This reverts commit a1bf53c2643ae2e281d5d2a80044bf9ec47a3c07.

--- background.js
+++ background.js
@@ -488,12 +488,13 @@ const SendLater = {
       // This _may_ be fixed in TB129, so we may eventually be able to remove
       // the delay, but possibly not if the root cause can't be identified. See
       // https://bugzilla.mozilla.org/show_bug.cgi?id=1909111 .
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      for (let folder of SendLater.draftsToCompact) {
-        SLStatic.debug("Compacting folder:", folder);
-        await messenger.SL3U.compactFolder(folder);
-      }
-      SendLater.draftsToCompact.length = 0;
+      setTimeout(async () => {
+        for (let folder of SendLater.draftsToCompact) {
+          SLStatic.debug("Compacting folder:", folder);
+          await messenger.SL3U.compactFolder(folder);
+        }
+        SendLater.draftsToCompact.length = 0;
+      }, 1000);
     } else if (SendLater.draftsToCompact.length) {
       SLStatic.debug("Not compacting folders, preference is disabled");
       SendLater.draftsToCompact.length = 0;
